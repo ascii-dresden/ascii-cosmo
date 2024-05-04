76e2991dd5ec30cf53621c958b2fc4b254351073
Johannes Kliemann <jk@jkliemann.de>
2024-05-04 22:09:11 +0200

    Add LED demo

--- firmware/ascii-cosmo/src/main.rs
+++ firmware/ascii-cosmo/src/main.rs
@@ -1,3 +1,5 @@
+use esp_idf_hal::{delay::FreeRtos, gpio::PinDriver, peripherals::Peripherals};
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -7,4 +9,11 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+    let peripherals = Peripherals::take().expect("Failed to take peripherals");
+    let mut led = PinDriver::output(peripherals.pins.gpio9).expect("Failed to create led driver");
+    loop {
+        led.toggle().expect("Failed to toggle led");
+        FreeRtos::delay_ms(500);
+    }
 }
